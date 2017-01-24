08f1a5849f9e8e37762cdb4afa7a2bbd1bd98eef
Nicola Giancecchi <nicorsm@me.com>
2017-01-24 22:46:09 +0100

    first working copy

--- routes/webservice.js
+++ routes/webservice.js
@@ -1,58 +1,125 @@
 var express = require('express');
 var router = express.Router();
-
 var cheerio = require('cheerio');
 var request = require('request');
+var bodyParser = require('body-parser');
+
+var Law = require("../models/law.js");
+var Attachment = require("../models/attachment.js");
+var Utils = require("../utils/utils.js");
 
 const querystring = require('querystring');
 var baseUrl = "http://www.consigliograndeegenerale.sm";
 var url = baseUrl + "/on-line/home/archivio-leggi-decreti-e-regolamenti.html";
 var documentsBaseUrl = baseUrl + "/on-line/home/archivio-leggi-decreti-e-regolamenti/";
-var Law = require("../models/law.js");
-var Attachment = require("../models/attachment.js");
 
-/* GET users listing. */
-router.get('/laws/:search', function (req, res, next) {
 
-  var term = req.params.search;
 
-  url += "?" + querystring.stringify({
+router.use(bodyParser.json()); // support json encoded bodies
+router.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+router.post('/laws', function (req, res, next) {
+
+  var title = req.body.title;
+  console.log(title)
+
+  var pageNumber = req.body.pageNumber;
+  if(pageNumber == null){
+    pageNumber = 1;
+  }
+  console.log(pageNumber)
+  
+  var requestType = req.body.type;
+  var type = "";
+  var currentType = "";
+  
+  var documentTypes = Utils.documentTypes;
+  for(var i = 0; i < documentTypes.length; i++){
+    var element = documentTypes[i];
+    if(element["short"] == requestType) {
+      type = element["parameter"];
+      break;
+    }
+  }
+  console.log(type)
+
+  var lawNumber = req.body.lawNumber;
+  console.log(lawNumber)
+
+  var lawYear = req.body.lawYear;
+  console.log(lawYear)
+
+  var lawStartingYear = req.body.lawStartingYear;
+  console.log(lawStartingYear)
+
+  var lawEndingYear = req.body.lawEndingYear;
+  console.log(lawEndingYear)
+
+  var day = req.body.day;
+  console.log(day)
+
+  var month = req.body.month;
+  console.log(month)
+
+  var year = req.body.year;
+  console.log(year)
+
+  var documentContent = req.body.documentContent;
+  console.log(documentContent)
+
+  var postUrl = url + "?" + querystring.stringify({
 
     "P0_path": "/home/tomcat/indicizzazione/indexleggi",
     "P0_paginazione": "15",
-    "P0_pagina": "1",
+    "P0_pagina": pageNumber,
     "P0_orderBy": "index_lucene,data_ordered,numero",
     "P0_order": "asc,desc,desc",
     "P0_operatorMustBe": "yes",
-    "P0_title": term,
-    "P0_tipo": "+Legge",
+    "P0_title": title,
+    "P0_tipo": type,
     "P0_operatorMustBe": "yes",
-    "P0_numero": "",
-    "P0_anno": "",
-    "annoiniziale": "",
-    "annofinale": "",
-    "P0_data_gg": "",
-    "P0_data_mm": "",
-    "P0_data_aa": "",
-    "P0_document": "",
+    "P0_numero": lawNumber,
+    "P0_anno": lawYear,
+    "annoiniziale": lawStartingYear,
+    "annofinale": lawEndingYear,
+    "P0_data_gg": day,
+    "P0_data_mm": month,
+    "P0_data_aa": year,
+    "P0_document": documentContent,
     "P0_data_ordered": "",
     "indicericerca": "-1",
-    "x": "5",
-    "y": "11"
+    "x": "0",
+    "y": "0"
   });
 
+  console.log(postUrl);
+  
   request({
     method: 'GET',
-    url: url
+    url: postUrl,
+    encoding: 'binary'
   }, function (err, response, body) {
-    if (err) return console.error(err);
+    if (err){
+
+      var response = {
+        "success":false,
+        "count":0,
+        "cgegUrl":postUrl,
+        "message":"There was a problem while fetching data from CGeG website.",
+        "data":[]
+      }
+
+      res.send(response);
+      return;
+    } 
 
     var $ = cheerio.load(body);
 
     var laws = [];
     var current = null;
-
-    $('#centercol').children().each(function (i, elem) {
+    var disclaimer = $(".avviso").first().text();
+    
+    $("#centercol").children().each(function (i, elem) {
       var node = $(this);
 
       var id = node.attr("id");
@@ -66,12 +133,6 @@ router.get('/laws/:search', function (req, res, next) {
         current = new Law();
         current.title = node.children().closest("h3").text();
         current.link = baseUrl + node.children().closest("div").children().closest("a").attr("href");
-
-        //find($(".btn-link fr")).closest("a").attr("href");
-        //console.log(node.find($(".btn-link fr")));
-
-        //current.link = "";
-
       } else if (current != null) {
         if (node.attr("class") == "fl") {
           //Document description
@@ -100,41 +161,42 @@ router.get('/laws/:search', function (req, res, next) {
           });
 
         }
-        
 
-        if (node == $("br")) {
+        if (node[0].name =="br") {
           //Closing current page and pushing the last element to the array
           laws.push(current);
+          current = null;
         }
-
       }
-
     });
 
+    var response = {
+      "success":true,
+      "count":laws.length,
+      "cgegUrl":postUrl,
+      "message":"Laws fetched successfully.",
+      "disclaimer":disclaimer,
+      "data":laws
+    }
 
-    res.send(laws);
-
-    console.log(laws);
+    res.send(response);
+    console.log(response);
 
   });
 
 });
 
+
+router.get('/configuration', function (req, res, next) {
+
+
+    var response = {
+      "success":true,
+      "documentTypes":Utils.documentTypes
+    };
+
+    res.send(response);
+  
+});
+
 module.exports = router;
-
-
-
-  /*
-
-Dati HTML:
-
-`html -> body -> div[#container] -> div[#page] -> div[#columns] -> div[#centercol]`
-
-- Titolo: `div[#card_NNNNNNNN] -> h3`
-- Link documento in PDF: `div[#card_NNNNNNNN] -> div[.btn-link fr] -> a.href` (da aggiungere in testa URL base)
-- Dati sul documento: `div[.fl]` senza link `a`
-- Valutazione ricerca: `div[.fr] -> a` (1: buono, 0.8: medio)
-- Altri documenti utili: `div[.list1 intest] -> ul -> li (foreach) -> a` (da prendere `href` e contenuto)
-
-Termina TUTTO con `<br/>`
-*/
---
+++ utils/utils.js
@@ -0,0 +1,104 @@
+function Utils() {};
+
+Utils.documentTypes = [
+        {
+            "short": "law",
+            "parameter": "+Legge",
+            "label_it": "Legge",
+            "label_en": "Law"
+        },
+        {
+            "short": "constitutionalLaw",
+            "parameter": "+Legge AND +Costituzionale",
+            "label_it": "Legge Costituzionale",
+            "label_en": "Constitutional Law"
+        },
+        {
+            "short": "qualifiedLaw",
+            "parameter": "+Legge AND +Qualificata",
+            "label_it": "Legge Qualificata",
+            "label_en": "Qualified Law"
+        },
+        {
+            "short": "lawConstitutionalRevision",
+            "parameter": "+Legge AND +Revisione AND +Costituziuonale",
+            "label_it": "Legge di Revisione Costituzionale",
+            "label_en": "Law of Constitutional Revision"
+        },
+        {
+            "short": "decree",
+            "parameter": "+Decreto",
+            "label_it": "Decreto",
+            "label_en": "Decree"
+        },
+        {
+            "short": "regencyDecree",
+            "parameter": "+Decreto AND +Reggenziale",
+            "label_it": "Decreto Reggenziale",
+            "label_en": "Regency Decree"
+        },
+        {
+            "short": "legislativeDecree",
+            "parameter": "+Decreto AND +Legge",
+            "label_it": "Decreto Legge",
+            "label_en": "Legislative Decree"
+        },
+        {
+            "short": "boardDecree",
+            "parameter": "+Decreto AND +Consiliare",
+            "label_it": "Decreto Consiliare",
+            "label_en": "Board Decree"
+        },
+        {
+            "short": "delegatedDecree",
+            "parameter": "+Decreto AND +Delegato",
+            "label_it": "Decreto Delegato",
+            "label_en": "Delegated Decree"
+        },
+        {
+            "short": "regulations",
+            "parameter": "+Regolamento",
+            "label_it": "Regolamento",
+            "label_en": "Regulations"
+        },
+        {
+            "short": "notification",
+            "parameter": "+Notifica",
+            "label_it": "Notifica",
+            "label_en": "notification"
+        },
+        {
+            "short": "statute",
+            "parameter": "+Statuto",
+            "label_it": "Statuto",
+            "label_en": "statute"
+        },
+        {
+            "short": "memorandum",
+            "parameter": "+Verbale",
+            "label_it": "Verbale",
+            "label_en": "Memorandum"
+        },
+        {
+            "short": "ordinance",
+            "parameter": "+Ordinanza",
+            "label_it": "Ordinanza",
+            "label_en": "Ordinance"
+        },
+        {
+            "short": "errata",
+            "parameter": "+Errata AND +Corrige",
+            "label_it": "Errata Corrige",
+            "label_en": "Errata"
+        },
+        {
+            "short": "undefined",
+            "parameter": "+Non AND definito",
+            "label_it": "Non definito",
+            "label_en": "Undefined"
+        }
+
+
+    ];
+
+module.exports = Utils;
