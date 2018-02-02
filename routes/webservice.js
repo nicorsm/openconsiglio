4aeef872baa1f7e893424088ed03bf03ba04ad9d
Nicola <nicorsm@me.com>
2018-02-02 01:16:46 +0100

    webservice refactoring

--- routes/webservice.js
+++ routes/webservice.js
@@ -13,25 +13,145 @@ var baseUrl = "https://www.consigliograndeegenerale.sm";
 var url = baseUrl + "/on-line/home/archivio-leggi-decreti-e-regolamenti.html";
 var documentsBaseUrl = baseUrl + "/on-line/home/archivio-leggi-decreti-e-regolamenti/";
 
-
-
 router.use(bodyParser.json()); // support json encoded bodies
 router.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+/*
+  Main endpoints
+*/
+
 router.post('/laws', function (req, res, next) {
 
-  var title = req.body.title;
-  console.log(title)
+  var postUrl = buildURL(req.body);
+  console.log(postUrl);
+  
+  request({
+    method: 'GET',
+    url: postUrl,
+    encoding: 'binary'
+  }, function (err, response, body) {
+    if (err){
 
-  var pageNumber = req.body.pageNumber;
+      var response = {
+        "success":false,
+        "count":0,
+        "cgegUrl":postUrl,
+        "error":err,
+        "message":"There was a problem while fetching data from CGeG website.",
+        "data":[]
+      }
+
+      res.send(response);
+      return;
+    } 
+
+    var xmlBody = cheerio.load(body);
+
+    var disclaimer = xmlBody(".avviso").first().text();
+    var laws = fetchLawsFromBody(xmlBody);
+
+    var response = {
+      "success":true,
+      "count":laws.length,
+      "cgegUrl":postUrl,
+      "message":"Laws fetched successfully.",
+      "disclaimer":disclaimer,
+      "data":laws
+    }
+
+    res.send(response);
+    console.log(response);
+
+  });
+
+});
+
+router.get('/configuration', function (req, res, next) {
+
+    var response = {
+      "success":true,
+      "documentTypes":Utils.documentTypes
+    };
+
+    res.send(response);
+});
+
+module.exports = router;
+
+/*
+  Methods for composing the URL and fetching data from the received body..
+*/
+
+function fetchLawsFromBody(body) {
+
+  var laws = [];
+  var current = null;
+
+  body("#centercol").children().each(function (i, elem) {
+    var node = body(this);
+
+    var id = node.attr("id");
+
+    if (id != null && id.startsWith("card_")) {
+
+      if (current != null) {
+        laws.push(current);
+      }
+
+      current = new Law();
+      current.id = parseInt(id.replace("card_", ""))
+      current.title = node.children().closest("h3").text();
+      current.link = baseUrl + node.children().closest("div").children().closest("a").attr("href");
+    } else if (current != null) {
+      if (node.attr("class") == "fl") {
+        //Document description
+        current.description = node.text().replace("(Visualizza dettagli)","")
+        .replace(/\xa0/g, " ")
+        .replace(/[^\x00-\x7F]/g, "");
+      }
+
+      if (node.attr("class") == "fr") {
+        //Document rating
+        current.rating = parseFloat(node.find("a").text());
+      }
+
+      if(node.attr("class") == "list1 intest") {
+        
+        current.attachments = new Array();
+        var ul = node.children().first()
+        ul.children().each(function (i,elem) {
+          var li = $(this);
+          var a = li.children("a");
+          var attachment = new Attachment();
+          attachment.title = a.text();
+          attachment.link = documentsBaseUrl + a.attr("href");
+          current.attachments.push(attachment);
+        });
+
+      }
+
+      if (node[0].name =="br") {
+        //Closing current page and pushing the last element to the array
+        laws.push(current);
+        current = null;
+      }
+    }
+  });
+
+  return laws;
+}
+
+
+function buildURL(body) {
+
+  var requestType = body.type;
+  var title = body.title;
+  var pageNumber = body.pageNumber;
   if(pageNumber == null){
     pageNumber = 1;
   }
-  console.log(pageNumber)
-  
-  var requestType = req.body.type;
+
   var currentType = "";
-  
   var documentTypes = Utils.documentTypes;
   var type = "";
   for(var i = 0; i < documentTypes.length; i++){
@@ -41,31 +161,15 @@ router.post('/laws', function (req, res, next) {
       break;
     }
   }
-  console.log(type)
 
-  var lawNumber = req.body.lawNumber;
-  console.log(lawNumber)
-
-  var lawYear = req.body.lawYear;
-  console.log(lawYear)
-
-  var lawStartingYear = req.body.lawStartingYear;
-  console.log(lawStartingYear)
-
-  var lawEndingYear = req.body.lawEndingYear;
-  console.log(lawEndingYear)
-
-  var day = req.body.day;
-  console.log(day)
-
-  var month = req.body.month;
-  console.log(month)
-
-  var year = req.body.year;
-  console.log(year)
-
-  var documentContent = req.body.documentContent;
-  console.log(documentContent)
+  var lawNumber = body.lawNumber;
+  var lawYear = body.lawYear;
+  var lawStartingYear = body.lawStartingYear;
+  var lawEndingYear = body.lawEndingYear;
+  var day = body.day;
+  var month = body.month;
+  var year = body.year;
+  var documentContent = body.documentContent;
 
   var postUrl = url + "?" + querystring.stringify({
 
@@ -92,112 +196,5 @@ router.post('/laws', function (req, res, next) {
     "y": "0"
   });
 
-  console.log(postUrl);
-  
-  request({
-    method: 'GET',
-    url: postUrl,
-    encoding: 'binary'
-  }, function (err, response, body) {
-    if (err){
-
-      var response = {
-        "success":false,
-        "count":0,
-        "cgegUrl":postUrl,
-        "error":err,
-        "message":"There was a problem while fetching data from CGeG website.",
-        "data":[]
-      }
-
-      res.send(response);
-      return;
-    } 
-
-    var $ = cheerio.load(body);
-
-    var laws = [];
-    var current = null;
-    var disclaimer = $(".avviso").first().text();
-    
-    $("#centercol").children().each(function (i, elem) {
-      var node = $(this);
-
-      var id = node.attr("id");
-
-      if (id != null && id.startsWith("card_")) {
-
-        if (current != null) {
-          laws.push(current);
-        }
-
-        current = new Law();
-        current.id = parseInt(id.replace("card_", ""))
-        current.title = node.children().closest("h3").text();
-        current.link = baseUrl + node.children().closest("div").children().closest("a").attr("href");
-      } else if (current != null) {
-        if (node.attr("class") == "fl") {
-          //Document description
-          current.description = node.text().replace("(Visualizza dettagli)","")
-          .replace(/\xa0/g, " ")
-          .replace(/[^\x00-\x7F]/g, "");
-        }
-
-        if (node.attr("class") == "fr") {
-          //Document rating
-          current.rating = parseFloat(node.find("a").text());
-        }
-
-        if(node.attr("class") == "list1 intest") {
-          
-          current.attachments = new Array();
-          var ul = node.children().first()
-          ul.children().each(function (i,elem) {
-            var li = $(this);
-            var a = li.children("a");
-            var attachment = new Attachment();
-            attachment.title = a.text();
-            attachment.link = documentsBaseUrl + a.attr("href");
-            current.attachments.push(attachment);
-          });
-
-        }
-
-        if (node[0].name =="br") {
-          //Closing current page and pushing the last element to the array
-          laws.push(current);
-          current = null;
-        }
-      }
-    });
-
-    var response = {
-      "success":true,
-      "count":laws.length,
-      "cgegUrl":postUrl,
-      "message":"Laws fetched successfully.",
-      "disclaimer":disclaimer,
-      "data":laws
-    }
-
-    res.send(response);
-    console.log(response);
-
-  });
-
-});
-
-
-router.get('/configuration', function (req, res, next) {
-
-
-    var response = {
-      "success":true,
-      "documentTypes":Utils.documentTypes
-    };
-
-    res.send(response);
-  
-});
-
-module.exports = router;
+  return postUrl;
+}
